cf78dd95b749151dd87f4d95d41d77efe66975b6
jiyoung <jiyoung-park@live.nmit.ac.nz>
2020-03-16 14:37:09 +1300

    practice

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const height = require('height');
 let a = 5;
 let b = 5;
 
@@ -50,3 +51,44 @@ switch (person) {
     console.log('Hi');
     break;
 }
+
+const foo = -1;
+switch (foo) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+    console.log('I like this number');
+    break;
+  case -1:
+  case -2:
+    console.log('I love this number');
+    break;
+  default:
+    console.log('I don\'t like this number.');
+}
+
+const goo = 0;
+let output = '';
+switch (goo) {
+  case 0:
+    output += 'where ';
+  case 1:
+    output += 'are ';
+    output += ' you ';
+  case 2:
+    output += 'from ';
+  case 3:
+    output += '!!';
+  case 4:
+    output += '?';
+    console.log(output);
+    break;
+  case 5:
+    output += '!';
+    console.log(output);
+    break;
+  default:
+    console.log('Please pick a number from 0 to 5!');
+}
+
