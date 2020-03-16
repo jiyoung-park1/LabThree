7b9af8cc5e3206adabc3f7906fd7cc55f12a2603
jiyoung <jiyoung-park@live.nmit.ac.nz>
2020-03-17 10:30:04 +1300

    class Acticity

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-var x = 47;
+var x = 20;
 var y;
 
-if (x >= 85) {
-  console.log("A+");
-  y = 1;
-} else if ( x >= 80){
-  console.log("A");
-  y = 1;
-} else if( x >= 75){
-  console.log("A-");
-  y = 1;
-} else if (x >= 70){
-  console.log("B+");
-  y = 2;
-} else if (x >= 65){
-  console.log("B");
-  y = 2;
+if(x >= 75){
+ y = 1;
 } else if(x >= 60){
-  console.log("B-");
   y = 2;
-} else if (x >= 55){;
-  console.log("C+");
+} else if(x >= 50){
   y = 3;
-} else if (x >= 50){
-  console.log("C");
-  y = 3;
-} else if (x >= 40){
-  console.log("D");
-  y = 4;
-} else if(x >= 0){
-  console.log("E");
-  y = 4;
 } else{
-   console.log("please write a number from 0 to 100");
+  y = 4;
 }
 
 switch (y) {
-  case 1:
-    console.log("Pass with Distinction");
-    break;
-  case 2:
-    console.log("Pass with Merit");
-    break;
-  case 3:
+  case(1):
+   console.log("Pass with Distinction");
+   if( x >= 85){
+     console.log("A+");
+   } else if(x >= 80){
+     console.log("A");
+   } else {
+     console.log("A-");
+   }
+   break;
+
+  case(2):
+   console.log("Pass with merit");
+   if(x >= 70){
+     console.log("B+");
+   } else if(x >= 65){
+     console.log("B");
+   }else {
+     console.log("B-");
+   }
+   break;
+  
+   case(3):
     console.log("Pass");
+    if(x >= 55){
+      console.log("C+");
+    } else{
+      console.log("c");
+    }
     break;
-  case 4:
-    console.log("Fail grades");
-    break;
-  default:
-    console.log("your description")
+
+    case(4):
+     console.log("Fail grages");
+     if(x >= 40){
+       console.log("D");
+     } else{
+       console.log("E");
+     }
+     break;
+
+     default:
+       console.log("please write a number from 0 to 100")
 }
